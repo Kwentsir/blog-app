90282d68e55603fc9902ff8e4772713313466d71
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-08-24 15:36:57 +0000

    Update Schema

--- db/schema.rb
+++ db/schema.rb
@@ -19,14 +19,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_124921) do
     t.datetime "updated_at", null: false
     t.text "text"
     t.bigint "author_id"
+    t.bigint "post_id"
     t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id"
+    t.bigint "post_id"
     t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -49,7 +53,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_124921) do
     t.integer "posts_counter"
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
