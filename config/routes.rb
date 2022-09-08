5db7664f832a2d75f3524c0d66e708ffbc1f1578
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-09-08 19:16:10 +0000

    Clean code and fix errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 root to: 'users#index'
-get 'likes/create'
-get 'comment/new'
-get 'comment/create'
+
 resources :users, only: [:index, :show] do
   resources :posts, only: [:index, :show, :new, :create] do
     resources :comments, only: [:index, :create]
