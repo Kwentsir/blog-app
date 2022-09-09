d4549e22fc88991a45eb92b4e73aed974fb6d419
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-09-09 12:31:33 +0000

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 root to: 'users#index'
 
 resources :users, only: [:index, :show] do
-  resources :posts, only: [:index, :show, :new, :create] do
-    resources :comments, only: [:index, :create]
-    resources :likes, only: [:create]
+  resources :posts, only: [:index, :show, :new, :create, :destroy] do
+    resources :comments, only: [:index, :create, :destroy]
+    resources :likes, only: [:create, :destroy]
   end
 end
 end
