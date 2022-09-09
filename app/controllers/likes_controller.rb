e1b10afbe4957d79321035514db4f1dc2ba11fd1
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-09-09 12:20:25 +0000

    Clean code

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,6 @@
 class LikesController < ApplicationController
   def create
-    @user = Current.user
+    @user = current_user
     @post = Post.includes(:author).find(params[:post_id])
 
     @already_liked = Like.where(author: @user, post: @post)
@@ -20,7 +20,7 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    @like = Current.user.likes.last
+    @like = current_user.likes.last
     @like.destroy
     @post = @like.post
     flash[:notice] = 'You unlike this post'
